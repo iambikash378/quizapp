c9e155e91a0daaa6f06fbe67410fc41f0805e97e
adbik <077bei015.bikash@pcampus.edu.np>
2025-05-25 11:59:09 +0545

    added sample questions

---
+++ src/data/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        "question": "What is the capital of Belgium?",
+        "options": ["Brussels", "Kathmandu", "Ohio", "Oslo"],
+        "correct": "Brussels"
+    },
+
+    {
+        "question": " Who discovered America ?",
+        "options":["Marcopolo", "Columbus", "Uncle Sam", "Bob Marley"],
+        "correct": "Columbus"
+    },
+
+];
+
+
+export default questions;
