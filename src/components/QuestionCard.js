4b6e32e4d3b4d10e1274813431a38c16c3c7ff8a
adbik <077bei015.bikash@pcampus.edu.np>
2025-05-25 13:39:01 +0545

    Added Question Component

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function QuestionCard({question, options, onSelect}){
+    return(
+        <div>
+            <h2> {question} </h2>
+            {options.map((option, index) => (
+                <button key = {index} onClick = {() => onSelect(option) }> 
+                    {option} 
+                </button>
+            )
+            )};
+        </div>
+
+    );
+}
+
+
+
+
+export default QuestionCard;
+
