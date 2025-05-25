96ac962b2fd0310f1ef0bb3e2f6c09414594a8f7
adbik <077bei015.bikash@pcampus.edu.np>
2025-05-25 14:15:24 +0545

    Wrote App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import QuestionCard from './components/QuestionCard';
+
+import questions from './data/questions';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const[currentIndex, setCurrentIndex] = useState(0);
+  const[score, setScore] = useState(0);
+  const[hasStarted, setHasStarted] = useState(false);
+  const[hasFinished, setHasFinished] = useState(false);
+
+  const handleAnswer = (selected_option) => {
+    if(selected_option == questions[currentIndex].answer) {
+      setScore(score+1);
+    }
+
+    const NextIndex = currentIndex + 1;
+
+    if (NextIndex < questions.length){
+      setCurrentIndex(NextIndex);
+    }
+    else {
+      setHasFinished(true);
+    }
+  };
+
+
+  return(
+    <div>
+      {
+        !hasFinished ? (
+          <QuestionCard question = {questions[currentIndex].question}
+          options = {questions[currentIndex].options}
+          onSelect = {handleAnswer} />
+        ):(
+          <div>
+            <h2> Quiz Finished </h2>
+            <p>Your score is {score} / {questions.length} </p>
+          </div>
+        );
+      }
     </div>
-  );
-}
+  )
+};
+
 
 export default App;
